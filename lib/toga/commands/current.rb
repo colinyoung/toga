77521dcda4ca23ff48dc1375aa3f327d1c84d37e
Colin Young <me@colinyoung.com>
2012-05-16 18:50:22 -0500

    Add later command and current command.

--- lib/toga/commands/current.rb
+++ lib/toga/commands/current.rb
@@ -5,7 +5,18 @@ module Toga
     class Current < List
       
       def self.run!(*args)
-        super [:current]
+        if !args.first || args.first.count == 0
+          return super [:current]
+        end
+
+        # Make a task current.
+        prefix = args.join(' ')
+        full = Togafile.remove_from_group :later, prefix
+        if !full
+          return error "Wasn't found."
+        end
+        Togafile.append_to_group :current, full
+        puts "Moved '#{full}' to current tasks."
       end
     end
   end
