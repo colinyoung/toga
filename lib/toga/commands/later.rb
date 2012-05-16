f05b686ce3cfee084938250bd2008aa5a2eb4815
Colin Young <me@colinyoung.com>
2012-05-16 02:48:59 -0500

    Add  command.

---
+++ lib/toga/commands/later.rb
@@ -0,0 +1,11 @@
+module Toga
+  module Commands
+    class Later < Command
+      
+      def self.run!(*args)
+        Togafile.append_to_group :later, args.join(' ')
+        puts Togafile.lines_in_group :later
+      end
+    end
+  end
+end
