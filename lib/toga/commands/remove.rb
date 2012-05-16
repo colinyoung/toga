a6fc7f0c45030cc709b9a08708dfc9e15fc94d4e
Colin Young <me@colinyoung.com>
2012-05-16 17:55:19 -0500

    Clear a task I'm not going to do anymore.

--- lib/toga/commands/remove.rb
+++ lib/toga/commands/remove.rb
@@ -3,13 +3,9 @@ module Toga
     class Remove < Command
       
       def self.run!(*args)
-        out = ""
-        
         removed = Togafile.remove_from_group :current, args.join(' ')
-        out << removed ? "Removed '#{removed}'.\r\n\r\n" : ""
-        out << Togafile.lines_in_group(:current)
-        
-        out
+        puts removed ? "Removed '#{removed}'.\r\n\r\n" : ""
+        Togafile.lines_in_group(:current)
       end
     end
   end
